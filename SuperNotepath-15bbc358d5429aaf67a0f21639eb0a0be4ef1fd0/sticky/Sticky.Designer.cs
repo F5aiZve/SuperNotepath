72942c30581b0a228dce6e33ce2eee4902c16e5a
Kelvin Cao <effecerit@gmail.com>
2017-05-15 13:47:00 +0700

    ok, ban nay co the tam chap nhan duoc

---
+++ SuperNotepath-15bbc358d5429aaf67a0f21639eb0a0be4ef1fd0/sticky/Sticky.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace sticky
+{
+    partial class Sticky
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.txtUser = new System.Windows.Forms.TextBox();
+            this.LblReminder = new System.Windows.Forms.LinkLabel();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // txtUser
+            // 
+            this.txtUser.AcceptsReturn = true;
+            this.txtUser.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtUser.BackColor = System.Drawing.SystemColors.Info;
+            this.txtUser.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtUser.Location = new System.Drawing.Point(0, 2);
+            this.txtUser.Multiline = true;
+            this.txtUser.Name = "txtUser";
+            this.txtUser.Size = new System.Drawing.Size(286, 260);
+            this.txtUser.TabIndex = 0;
+            this.txtUser.Text = "write your note here.";
+            // 
+            // LblReminder
+            // 
+            this.LblReminder.AutoSize = true;
+            this.LblReminder.Location = new System.Drawing.Point(194, 248);
+            this.LblReminder.Name = "LblReminder";
+            this.LblReminder.Size = new System.Drawing.Size(83, 13);
+            this.LblReminder.TabIndex = 1;
+            this.LblReminder.TabStop = true;
+            this.LblReminder.Text = "Remind me later";
+            this.LblReminder.TextAlign = System.Drawing.ContentAlignment.BottomRight;
+            this.LblReminder.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.LblReminder_LinkClicked);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Sticky
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
+            this.BackColor = System.Drawing.SystemColors.Info;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.LblReminder);
+            this.Controls.Add(this.txtUser);
+            this.ForeColor = System.Drawing.Color.CornflowerBlue;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
+            this.Name = "Sticky";
+            this.Opacity = 0.85D;
+            this.ShowInTaskbar = false;
+            this.Text = "MySticky";
+            this.TopMost = true;
+            this.Deactivate += new System.EventHandler(this.Form1_Deactivate);
+            this.Load += new System.EventHandler(this.Sticky_Load);
+            this.LocationChanged += new System.EventHandler(this.Form1_LocationChanged);
+            this.SizeChanged += new System.EventHandler(this.Form1_SizeChanged);
+            this.VisibleChanged += new System.EventHandler(this.Form1_VisibleChanged);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtUser;
+        private System.Windows.Forms.LinkLabel LblReminder;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ SuperNotepath-a5513be15946bd1ede68ac1ab46c93cb434cc7aa/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,298 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing.Text;
+using sticky;
+
+namespace WindowsFormsApplication1
+{
+    public partial class FrmMain : Form
+    {
+        string fileName = "NoName";
+        FontStyle style = FontStyle.Regular;
+        Font font;
+        private int NumberofSticky = -1;
+        private List<Sticky> aStickyNote = new List<Sticky>();
+        public FrmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void SaveSetting()
+        {
+            Properties.Settings.Default.Location = this.Location;
+            Properties.Settings.Default.Height = this.Height;
+            Properties.Settings.Default.Width = this.Width;
+            Properties.Settings.Default.Save();
+        }
+
+        private void LoadSetting()
+        {
+            this.Location = Properties.Settings.Default.Location;
+            this.Height = Properties.Settings.Default.Height;
+            this.Width = Properties.Settings.Default.Width;
+        }
+
+        private void FrmMain_Load(object sender, EventArgs e)
+        {
+            this.Text = "Super Notepad - " + fileName;
+            LoadSetting();
+            for (int i = 1; i <= 120; i++)
+            {
+                tSCBSize.Items.Add(i);
+            }
+            InstalledFontCollection listFont = new InstalledFontCollection();
+            foreach (FontFamily font in listFont.Families)
+            {
+                tSCBFont.Items.Add(font.Name);
+            }
+        }
+
+        private void tSCBFont_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            FontFamily fa = new FontFamily(tSCBFont.Text);
+            Font f = new System.Drawing.Font(fa, float.Parse(tSCBSize.Text));
+            if (rTBMain.SelectedText.Length > 0)
+                rTBMain.SelectionFont = f;
+        }
+
+        private void tSCBSize_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            FontFamily fa = new FontFamily(tSCBFont.Text);
+            Font f = new System.Drawing.Font(fa, float.Parse(tSCBSize.Text));
+            if (rTBMain.SelectedText.Length > 0)
+                rTBMain.SelectionFont = f;
+        }
+
+        private void tSBtnBold_Click(object sender, EventArgs e)
+        {
+
+                    style = style ^ FontStyle.Bold;
+                    font = new System.Drawing.Font(new FontFamily(tSCBFont.Text), float.Parse(tSCBSize.Text), style);
+                    rTBMain.SelectionFont = font;
+        }
+
+        private void tSBtnItalic_Click(object sender, EventArgs e)
+        {
+
+            style = style ^ FontStyle.Italic;
+            font = new System.Drawing.Font(new FontFamily(tSCBFont.Text), float.Parse(tSCBSize.Text), style);
+            rTBMain.SelectionFont = font;
+
+        }
+
+        private void tSBtnUnderline_Click(object sender, EventArgs e)
+        {
+            style = style ^ FontStyle.Underline;
+            font = new System.Drawing.Font(new FontFamily(tSCBFont.Text), float.Parse(tSCBSize.Text), style);
+            rTBMain.SelectionFont = font;
+        }
+
+        private void tSBtnTextColor_Click(object sender, EventArgs e)
+        {
+            if (this.colorDialog1.ShowDialog() == DialogResult.OK)
+            {
+                rTBMain.SelectionColor = colorDialog1.Color;
+            }
+
+        }
+
+        private void tSBtnBackgroundColor_Click(object sender, EventArgs e)
+        {
+            if (this.colorDialog1.ShowDialog() == DialogResult.OK)
+            {
+                rTBMain.BackColor = colorDialog1.Color;
+            }
+        }
+
+        private void undoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            rTBMain.Undo();
+        }
+
+        private void redoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            rTBMain.Redo();
+        }
+
+        private void tSBtnUndo_Click(object sender, EventArgs e)
+        {
+            undoToolStripMenuItem.PerformClick();
+        }
+
+        private void tSBtnRedo_Click(object sender, EventArgs e)
+        {
+            redoToolStripMenuItem.PerformClick();
+        }       
+
+        private void newToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            DialogResult da;
+
+            da = MessageBox.Show("Do you want to save changes to ?" + fileName + " ?", "SuperNotepath", MessageBoxButtons.YesNoCancel, MessageBoxIcon.Warning);
+            if (da == DialogResult.Yes)
+            {
+                tSBtnSave_Click(sender, e);
+                rTBMain.Clear();
+            }
+            if (da == DialogResult.No)
+            {
+                rTBMain.Clear();
+                tSCBFont.Font = DefaultFont;
+                tSCBSize.Font = DefaultFont;
+                tSBtnBold.Checked = false;
+                tSBtnItalic.Checked = false;
+                tSBtnUnderline.Checked = false;
+                rTBMain.BackColor = Color.White;
+            }
+        }
+        private void tSBtnNew_Click(object sender, EventArgs e)
+        {
+            newToolStripMenuItem.PerformClick();
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if(rTBMain.Modified)
+            {
+                if(MessageBox.Show("Do you want to save changes to " + fileName + " ?", "SuperNotepath", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == System.Windows.Forms.DialogResult.OK)
+                {
+                    saveToolStripMenuItem_Click(null, null);
+                }
+            }
+           if(oFDOpen.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                fileName = oFDOpen.FileName;
+                rTBMain.Text = System.IO.File.ReadAllText(fileName);
+                this.Text = "Super Notepad - " + fileName;
+            }
+        }
+        private void tSBtnOpen_Click(object sender, EventArgs e)
+        {
+            openToolStripMenuItem.PerformClick();
+        }
+
+        private void tSBtnSave_Click(object sender, EventArgs e)
+        {
+            saveToolStripMenuItem.PerformClick();
+        }
+
+        private void saveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (fileName == "NoName")
+            {
+                saveAsToolStripMenuItem_Click(null, null);
+            }
+            else
+            {
+                System.IO.File.WriteAllText(fileName, rTBMain.Text);
+                this.Text = "Super Notepad - " + fileName;
+            }
+}
+
+        private void tSBtnCopy_Click(object sender, EventArgs e)
+        {
+            copyToolStripMenuItem.PerformClick();
+        }
+
+        private void tSBtnPaste_Click(object sender, EventArgs e)
+        {
+            pasteToolStripMenuItem.PerformClick();
+        }
+
+        private void tSBtnPinNote_Click(object sender, EventArgs e)
+        {
+            NumberofSticky++;
+            int x = NumberofSticky * 301;
+            int y = NumberofSticky * 300;
+            aStickyNote.Add(new Sticky(x, y));            
+            aStickyNote.Last().Location = new Point(x, y);
+            aStickyNote.Last().getTxtUser = rTBMain.Text;
+        }
+
+        private void tSBtnCut_Click(object sender, EventArgs e)
+        {
+            cutToolStripMenuItem.PerformClick();
+        }
+
+        private void saveAsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (sFDSave.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                fileName = sFDSave.FileName;
+                System.IO.File.WriteAllText(fileName, rTBMain.Text);
+                this.Text = "Super Notepad - " + fileName;
+            }
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void fontToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            fDFont.Font = rTBMain.Font;
+            if (fDFont.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                rTBMain.Font = fDFont.Font;
+            }
+        }
+
+        private void rrTBMain_TextChanged(object sender, EventArgs e)
+        {
+            if (rTBMain.Modified)
+            {
+                this.Text = "Super Notepad - " + fileName + "*";
+            }
+        }
+
+        private void cutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            rTBMain.Cut();
+        }
+
+        private void copyToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            rTBMain.Copy();
+        }
+
+        private void pasteToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            rTBMain.Paste();
+        }
+
+        private void selectAllToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            rTBMain.SelectAll();
+        }
+
+        private void cutToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            cutToolStripMenuItem.PerformClick();
+        }
+
+        private void wordWrapToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (wordWrapToolStripMenuItem.Checked)
+            {
+                rTBMain.WordWrap = true;
+            }
+            else
+            {
+                rTBMain.WordWrap = false;
+            }
+        }
+
+        private void FrmMain_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            SaveSetting();
+        }
+    }
+}
